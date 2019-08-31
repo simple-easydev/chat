1abb2ffa5f30cddf74fa0316aa721530fecffbfb
apple <heavy.sheng.yu@gmail.com>
2019-09-01 00:54:43 +0800

    update

--- www/scripts/hichat.js
+++ www/scripts/hichat.js
@@ -53,7 +53,7 @@ class HiChat{
         console.log(name, usertype);
         
         var that = this;
-        this.socket = io.connect("http://3.85.238.14:3000");
+        this.socket = io.connect();
         this.socket.on('connect', function() {
             document.getElementById('info').textContent = 'get yourself a nickname :)';
             if(usertype == "guest"){
